1a65c85ae474b4604a0c5fb115d15ff0a7b546d7
Szymon Tymcio <s.tymcio98@gmail.com>
2023-04-14 14:46:10 +0200

    update v1

--- src/components/imageLinkForm/ImageLinkForm.jsx
+++ src/components/imageLinkForm/ImageLinkForm.jsx
@@ -1,27 +1,29 @@
 import React from "react";
-
+import "../../styles/ImageLinkForm.css";
 export default function ImageLinkForm({ handleUrl, handleData, newData }) {
   return (
     <div>
       <p className="f3 fw5">
         The magical brain will detect any face on your picture. Let's try!
       </p>
-      <form onSubmit={handleUrl}>
-        <input
-          value={newData}
-          className="tc f5 pa2 w-70"
-          placeholder="paste url"
-          onChange={handleData}
-        />
-        <div>
-          <button
-            className="w-20 grow f5 link ph3 pv2 dib white bg-light-purple br3"
-            type="submit"
-          >
-            Detect
-          </button>
-        </div>
-      </form>
+      <div className="center shadow-5 pa3 m5 formstyle">
+        <form onSubmit={handleUrl} className="w-70">
+          <input
+            value={newData}
+            className="tc f5 pa2 w-70 br1 shadow-5"
+            placeholder="paste url"
+            onChange={handleData}
+          />
+          <div>
+            <button
+              className=" grow f5 link ph3 pv2 dib white bg-light-purple br1 shadow-5 ba b--gray"
+              type="submit"
+            >
+              Detect
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
