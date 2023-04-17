be3ea8f6a7c7c4356483d57c64bef5dbe3be29bc
Szymon Tymcio <s.tymcio98@gmail.com>
2023-04-17 21:26:00 +0200

    face recognition v1

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,88 @@ import ImageLinkForm from "./components/imageLinkForm/ImageLinkForm.jsx";
 import { useState } from "react";
 import ParticlesBg from "particles-bg";
 import Rank from "./components/rank/Rank";
+import FaceRecognition from "./components/faceRecognition/FaceRecognition";
+import axios from "axios";
+// console.log(tescik2);
 function App() {
   const [newData, setData] = useState("");
-  const [userData, setUserData] = useState([]);
+  const [newImage, setNewImage] = useState([]);
+
   const handleUrl = (event) => {
     event.preventDefault();
-    console.log("Current link: ", newData);
+    // console.log("Current link: ", newData);
+    processImage(newData);
     setData("");
   };
-  const handlerUser = () => {};
+
   const handleData = (event) => {
     const temp = event.target.value;
-    console.log("info: ", temp);
     setData(temp);
   };
+
+  const processImage = (url) => {
+    const PAT = import.meta.env.VITE_REACT_APP_PAT_KEY;
+    // Specify the correct user_id/app_id pairings
+    // Since you're making inferences outside your app's scope
+    const USER_ID = "sp3c";
+    const APP_ID = "test";
+    // Change these to whatever model and image URL you want to use
+    const MODEL_ID = "general-image-recognition";
+    const MODEL_VERSION_ID = "aa7f35c01e0642fda5cf400f543e7c40";
+    const IMAGE_URL = url;
+
+    const raw = JSON.stringify({
+      user_app_id: {
+        user_id: USER_ID,
+        app_id: APP_ID,
+      },
+      inputs: [
+        {
+          data: {
+            image: {
+              url: IMAGE_URL,
+            },
+          },
+        },
+      ],
+    });
+
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        Authorization: "Key " + PAT,
+      },
+      body: raw,
+    };
+
+    fetch(
+      "https://api.clarifai.com/v2/models/" +
+        MODEL_ID +
+        "/versions/" +
+        MODEL_VERSION_ID +
+        "/outputs",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setNewImage(result.outputs))
+      .catch((error) => console.log("error", error));
+    console.log(newImage);
+    // axios
+    //   .post(
+    //     "https://api.clarifai.com/v2/models/" +
+    //       MODEL_ID +
+    //       "/versions/" +
+    //       MODEL_VERSION_ID +
+    //       "/outputs",
+    //     requestOptions
+    //   )
+    //   .then((response) => response.data)
+    //   .then((response) => {
+    //     console.log("co to jest:", response);
+    //   });
+  };
+
   return (
     <div className="app">
       <NavigationBar className="navBar" />
@@ -31,9 +99,8 @@ function App() {
         handleData={handleData}
         newData={newData}
       />
-      {/*
-    <FaceRecognition/> */}
-      <ParticlesBg color="#ff0000" num={50} type="cobweb" bg={true} />
+      <FaceRecognition newImage={newImage} />
+      <ParticlesBg color="#FFFEFE" num={50} type="cobweb" bg={true} />
     </div>
   );
 }
---
+++ src/components/faceRecognition/FaceRecognition.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function FaceRecognition({ newImage }) {
+  return <div>sdsdsd</div>;
+}
